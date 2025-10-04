be99cb3d9841f84715c718433966ecbb9f130e44
Skgraphicvision <sanathavantrio@gmail.com>
2025-10-04 13:42:55 +0530

    feat: implement theme colors fonts

---
+++ src/theme/colors.ts
@@ -0,0 +1,13 @@
+export const colors = {
+   background: '#181818',
+   text: '#FFFFFF',
+   black: '#000000',
+   button: '#282828',
+   primary: '#FF660E',
+   secondary: '#47FFB8',
+   error: '#b10900ff',
+   border:'#3A3A3C',
+   inputText: '#E9EDEF',
+   input: '#201f23ff',
+   placeholder: '#A0A0A0'
+}
---
+++ src/theme/fonts.ts
@@ -0,0 +1,21 @@
+ export const FontFamily = {
+    regular: 'Montserrat-Regular',
+    medium: 'Montserrat-Medium',
+    semiBold: 'Montserrat-SemiBold',
+    bold: 'Montserrat-Bold',
+};
+
+export const FontSize = {
+    size_xs: 12,
+    size_sm: 14,
+    size_base: 16,
+    size_lg: 18,
+    size_xl: 20,
+    size_2xl: 24,
+    size_3xl: 30,
+    size_4xl: 36,
+    size_5xl: 48,
+    size_6xl: 60,
+    size_7xl: 72,
+};
+
